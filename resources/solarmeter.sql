0e756651f7fb11e92abbd5493ff8a5a2f24b2faa
Alexander Pohl <alex@ahpohl.com>
2021-04-16 19:14:58 +0200

    add state table

--- resources/solarmeter.sql
+++ resources/solarmeter.sql
@@ -4,6 +4,7 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 DROP TABLE IF EXISTS "input" CASCADE;
 DROP TABLE IF EXISTS "grid" CASCADE;
 DROP TABLE IF EXISTS "stats" CASCADE;
+DROP TABLE IF EXISTS "state" CASCADE;
 DROP TABLE IF EXISTS "sensors" CASCADE;
 
 CREATE TABLE "sensors" (
@@ -43,15 +44,27 @@ CREATE TABLE "grid" (
 CREATE TABLE "stats" (
   time TIMESTAMPTZ NOT NULL,
   sensor_id INTEGER NOT NULL,
-  temp_inverter DOUBLE PRECISION,
-  temp_booster DOUBLE PRECISION,
-  energy_total DOUBLE PRECISION,
+  inverter_temp DOUBLE PRECISION,
+  booster_temp DOUBLE PRECISION,
+  total_energy DOUBLE PRECISION,
+  CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
+);
+
+CREATE TABLE "state" (
+  time TIMESTAMPTZ NOT NULL,
+  sensor_id INTEGER NOT NULL,
+  global VARCHAR(50),
+  inverter VARCHAR(50),
+  channel_1 VARCHAR(50),
+  channel_2 VARCHAR(50),
+  alarm VARCHAR(50),
   CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
 );
 
 SELECT create_hypertable('input', 'time');
 SELECT create_hypertable('grid', 'time');
 SELECT create_hypertable('stats', 'time');
+SELECT create_hypertable('state', 'time');
 
 INSERT INTO sensors(id, serial, mfg_date, part_no, firmware, grid, inverter, type, transformer) VALUES 
 (1, '126014', 'May 2010', '3G79', 'C.0.2.2', 'Germany-VDE0126', 'PVI-4.2-OUTD', 'PV', false);
@@ -64,6 +77,9 @@ GRANT SELECT ON TABLE grid TO grafana;
 
 GRANT INSERT, SELECT ON TABLE stats TO nodejs;
 GRANT SELECT ON TABLE stats TO grafana;
+
+GRANT INSERT, SELECT ON TABLE state TO nodejs;
+GRANT SELECT ON TABLE state TO grafana;
 
 GRANT SELECT ON TABLE sensors TO nodejs;
 GRANT SELECT ON TABLE sensors TO grafana;
