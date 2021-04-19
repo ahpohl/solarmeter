3fa4b6e7871ee94548c9106a1c90bfd96df9de02
Alexander Pohl <alex@ahpohl.com>
2021-04-19 22:29:59 +0200

    alter schema to wide format

--- resources/solarmeter.sql
+++ resources/solarmeter.sql
@@ -1,11 +1,7 @@
 \c solarmeter
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
-DROP TABLE IF EXISTS "input" CASCADE;
-DROP TABLE IF EXISTS "grid" CASCADE;
-DROP TABLE IF EXISTS "stats" CASCADE;
-DROP TABLE IF EXISTS "state" CASCADE;
-DROP TABLE IF EXISTS "extended" CASCADE;
+DROP TABLE IF EXISTS "live" CASCADE;
 DROP TABLE IF EXISTS "sensors" CASCADE;
 
 CREATE TABLE "sensors" (
@@ -20,7 +16,7 @@ CREATE TABLE "sensors" (
   transformer BOOLEAN
 );
 
-CREATE TABLE "input" (
+CREATE TABLE "live" (
   time TIMESTAMPTZ NOT NULL,
   sensor_id INTEGER NOT NULL,
   voltage_1 DOUBLE PRECISION,
@@ -29,70 +25,25 @@ CREATE TABLE "input" (
   voltage_2 DOUBLE PRECISION,
   current_2 DOUBLE PRECISION,
   power_2 DOUBLE PRECISION,
-  CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
-);
-
-CREATE TABLE "grid" (
-  time TIMESTAMPTZ NOT NULL,
-  sensor_id INTEGER NOT NULL,
-  voltage DOUBLE PRECISION,
-  current DOUBLE PRECISION,
-  power DOUBLE PRECISION,
-  frequency DOUBLE PRECISION,
-  CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
-);
-
-CREATE TABLE "stats" (
-  time TIMESTAMPTZ NOT NULL,
-  sensor_id INTEGER NOT NULL,
+  grid_voltage DOUBLE PRECISION,
+  grid_current DOUBLE PRECISION,
+  grid_power DOUBLE PRECISION,
+  grid_frequency DOUBLE PRECISION,
   inverter_temp DOUBLE PRECISION,
   booster_temp DOUBLE PRECISION,
   total_energy DOUBLE PRECISION,
-  CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
-);
-
-CREATE TABLE "state" (
-  time TIMESTAMPTZ NOT NULL,
-  sensor_id INTEGER NOT NULL,
-  global VARCHAR(50),
-  inverter VARCHAR(50),
-  channel_1 VARCHAR(50),
-  channel_2 VARCHAR(50),
-  alarm VARCHAR(50),
-  CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
-);
-
-CREATE TABLE "extended" (
-  time TIMESTAMPTZ NOT NULL,
-  sensor_id INTEGER NOT NULL,
   vbulk DOUBLE PRECISION,
   riso DOUBLE PRECISION,
   CONSTRAINT sensor_id FOREIGN KEY (sensor_id) REFERENCES sensors (id)
 );
 
-SELECT create_hypertable('input', 'time');
-SELECT create_hypertable('grid', 'time');
-SELECT create_hypertable('stats', 'time');
-SELECT create_hypertable('state', 'time');
-SELECT create_hypertable('extended', 'time');
+SELECT create_hypertable('live', 'time');
 
 INSERT INTO sensors(id, serial, mfg_date, part_no, firmware, grid, inverter, type, transformer) VALUES 
 (1, '126014', 'May 2010', '3G79', 'C.0.2.2', 'Germany-VDE0126', 'PVI-4.2-OUTD', 'PV', false);
 
-GRANT INSERT, SELECT ON TABLE input TO nodejs;
-GRANT SELECT ON TABLE input TO grafana;
-
-GRANT INSERT, SELECT ON TABLE grid TO nodejs;
-GRANT SELECT ON TABLE grid TO grafana;
-
-GRANT INSERT, SELECT ON TABLE stats TO nodejs;
-GRANT SELECT ON TABLE stats TO grafana;
-
-GRANT INSERT, SELECT ON TABLE state TO nodejs;
-GRANT SELECT ON TABLE state TO grafana;
-
-GRANT INSERT, SELECT ON TABLE extended TO nodejs;
-GRANT SELECT ON TABLE extended TO grafana;
+GRANT INSERT, SELECT ON TABLE live TO nodejs;
+GRANT SELECT ON TABLE live TO grafana;
 
 GRANT SELECT ON TABLE sensors TO nodejs;
 GRANT SELECT ON TABLE sensors TO grafana;
